739c24b65e7e684a80519c65651ebb073694669d
ding3820 <ding3820@gmail.com>
2021-06-28 16:20:38 +0800

    update comment and test code

--- non_local_block.py
+++ non_local_block.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class NonLocalBlock(nn.Module):
-    def __init__(self, in_channel, p=1, k=1, temperature=1e-3, device='cuda'):
+    def __init__(self, in_channel, p=4, k=4, temperature=1e-3, device='cuda'):
         super(NonLocalBlock, self).__init__()
         self.avgpool = nn.AvgPool2d(kernel_size=p, stride=p)
         self.topk = k
@@ -29,8 +29,8 @@ class NonLocalBlock(nn.Module):
     def forward(self, target, ref, ref_align):
         # b, c, H, W
         b, c, H, W = target.shape
-        p_height = target.shape[2] // self.block_size  # h
-        p_width = target.shape[3] // self.block_size  # w
+        p_height = H // self.block_size  # h
+        p_width = W // self.block_size  # w
         N = p_height * p_width
 
         # b, c, hw
@@ -42,25 +42,27 @@ class NonLocalBlock(nn.Module):
 
         # # b, hw, k
         topk_value, topk_indice = torch.topk(D_p, self.topk, dim=1, largest=False)
-        topk_value = topk_value.transpose(1, 2)
+        # topk_value = topk_value.transpose(1, 2)
         topk_indice = topk_indice.transpose(1, 2)
 
-        # b, hw, c, p^2
+        # b, c, H, W -> b, 1, h, w, c -> b, 1, h/p, w, c, p -> b, 1, h/p, w/p, c, p, p
+        # -> b, hw, c, p^2
+
         F_p_ref_align = ref_align.unfold(1, self.in_channel, self.in_channel
                                          ).unfold(2, self.block_size, self.block_size
                                                   ).unfold(3, self.block_size, self.block_size)
         F_p_ref_align = F_p_ref_align.reshape(b, -1, self.in_channel, self.block_size * self.block_size)
         F_p_ref = ref.unfold(1, self.in_channel, self.in_channel
-                                         ).unfold(2, self.block_size, self.block_size
-                                                  ).unfold(3, self.block_size, self.block_size)
+                             ).unfold(2, self.block_size, self.block_size
+                                      ).unfold(3, self.block_size, self.block_size)
         F_p_ref = F_p_ref.reshape(b, -1, self.in_channel, self.block_size * self.block_size)
         F_p_target = target.unfold(1, self.in_channel, self.in_channel
                                    ).unfold(2, self.block_size, self.block_size
                                             ).unfold(3, self.block_size, self.block_size)
         F_p_target = F_p_target.reshape(b, -1, self.in_channel, self.block_size * self.block_size)
 
-
-        # b, hw, c, k, p^2 -> b, hw, c, kp^2
+        # b, hw, c, p^2 -> b, hw, k, c, p^2
+        # ->b, hw, c, k, p^2 -> b, hw, c, kp^2
         topk_block_align = F_p_ref_align[torch.arange(b).unsqueeze(1).unsqueeze(2), topk_indice].transpose(2, 3).reshape(
             b, N, self.in_channel, -1)
         topk_block = F_p_ref[torch.arange(b).unsqueeze(1).unsqueeze(2), topk_indice].transpose(2, 3).reshape(
@@ -88,7 +90,7 @@ class NonLocalBlock(nn.Module):
 
 class NonLocal(nn.Module):
     def __init__(self, in_channel, inter_channels):
-        super(NonLocalMean, self).__init__()
+        super(NonLocal, self).__init__()
         self.in_channel = in_channel
         self.pool = nn.MaxPool2d(kernel_size=(2, 2))
 
@@ -101,7 +103,6 @@ class NonLocal(nn.Module):
         self.phi = nn.Conv2d(in_channel, inter_channels, 1, 1, 0)
 
     def forward(self, target, ref, ref_align):
-
         b, c, h, w = target.shape
         N = h * w
         # b, c, h/2*w/2
@@ -126,7 +127,6 @@ class NonLocal(nn.Module):
         return z
 
 
-
 if __name__ == '__main__':
     # img1 = Variable(torch.rand(1, 3, 720, 1280))
     # img2 = Variable(torch.rand(1, 3, 720, 1280))
@@ -135,32 +135,33 @@ if __name__ == '__main__':
     from torchvision.transforms import ToTensor, ToPILImage, RandomCrop
     import torchvision.transforms.functional as TF
 
-    img1 = Image.open("00000001.png").convert("RGB")
-    img2 = Image.open("00000002.png").convert("RGB")
-    img3 = Image.open("00000003.png").convert("RGB")
-    img4 = Image.open("00000004.png").convert("RGB")
-    
-    # i, j, h, w = RandomCrop.get_params(img1, output_size=(64, 64))
-    #
+    img1 = Image.open("00000005.png").convert("RGB")
+    img2 = Image.open("00000006.png").convert("RGB")
+    # img3 = Image.open("00000003.png").convert("RGB")
+    # img4 = Image.open("00000004.png").convert("RGB")
+
+    # i, j, h, w = RandomCrop.get_params(img1, output_size=(256, 256))
+    # #
     # img1 = TF.crop(img1, i, j, h, w)
     # img2 = TF.crop(img2, i, j, h, w)
     # img3 = TF.crop(img3, i, j, h, w)
     # img4 = TF.crop(img4, i, j, h, w)
-    
-    img1_down = torch.unsqueeze(ToTensor()(img1_down), dim=0)
-    img1 = torch.unsqueeze(ToTensor()(img1), dim=0)
-    img2 = torch.unsqueeze(ToTensor()(img2), dim=0)
-    img3 = torch.unsqueeze(ToTensor()(img3), dim=0)
-    img4 = torch.unsqueeze(ToTensor()(img4), dim=0)
 
-    in1 = torch.cat([img1, img3], dim=0)
-    in2 = torch.cat([img2, img4], dim=0)
+    img1 = torch.unsqueeze(ToTensor()(img1), dim=0).cuda()
+    img2 = torch.unsqueeze(ToTensor()(img2), dim=0).cuda()
+    # img3 = torch.unsqueeze(ToTensor()(img3), dim=0)
+    # img4 = torch.unsqueeze(ToTensor()(img4), dim=0)
 
-    model1 = NonLocalBlock(3, p=1, k=1, temperature=1e-3)
+    # in1 = torch.cat([img1, img3], dim=0)
+    # in2 = torch.cat([img2, img4], dim=0)
+
+    model1 = NonLocalBlock(3, p=10, k=4, temperature=1e-3).cuda()
     model2 = NonLocal(3, 3)
-    output = model(in2, in1, in1)
-    output = model(in2, in1, in1)
-
-    for b in range(2):
-        img = ToPILImage()(output[b])
-        img.save('save'+str(b)+'.jpg')
+    output = model1(img2, img1, img1)
+    # output = model2(in2, in1, in1)
+    img = ToPILImage()(output[0])
+    print(torch.sum(output - img1))
+    img.save('save' + str(0) + '.jpg')
+    # for b in range(2):
+    #     img = ToPILImage()(output[b])
+    #     img.save('save'+str(b)+'.jpg')
